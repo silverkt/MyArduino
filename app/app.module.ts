4b0a3f2ba559a1a7f34446375c0b8428ca752094
silver.sun <itisagift@yeah.net>
2017-01-22 17:04:18 +0800

    upd

---
+++ app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router';
+
+import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component'; 
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,  RouterModule.forRoot([
+                                                                    {
+                                                                      path: '',
+                                                                      redirectTo: '/dashboard',
+                                                                      pathMatch: 'full'
+                                                                    },
+                                                                    {
+                                                                      path: 'heroes',
+                                                                      component: HeroesComponent,                                                    
+                                                                    },
+                                                                    {
+                                                                      path: 'dashboard',
+                                                                      component: DashboardComponent,
+                                                                    }
+                                                                  ]) 
+  ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [HeroService],  
+})
+export class AppModule { 
+   
+}
